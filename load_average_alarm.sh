a41ddd4373138fb964595f14b03d34558ff9eda6
amdukf <amdukf@gmail.com>
2022-07-26 19:20:06 +0430

    cpu_load v3

--- load_average_alarm.sh
+++ load_average_alarm.sh
@@ -1,17 +1,7 @@
 #! /bin/bash
 
-#load=$(uptime | awk '{print $9}' | cut -d "," -f 1)
-#if (( "$load" >  0.03 ))
-#then
-#	echo "Alert , Alert"
-#else
-#	echo "good"
-#fi
-#
-#echo $load
-
-load=$(uptime | awk '{print $9}' | cut -d "," -f 1)
-cpu=0.5
+load=$(cat /proc/loadavg | awk '{print $1}')
+cpu=1
 check=$(echo $load '>' $cpu | bc -l )
 if (( $check == 1 ))
 then

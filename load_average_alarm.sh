cf7a983a7ff303985e6e47c6913bfddf18778f55
amdukf <amdukf@gmail.com>
2022-07-27 00:08:40 +0430

    cpu_load v4

--- load_average_alarm.sh
+++ load_average_alarm.sh
@@ -1,7 +1,8 @@
 #! /bin/bash
 
 load=$(cat /proc/loadavg | awk '{print $1}')
-cpu=1
+#cpu=1 # or you can use this instead of command that write in line 5 , for set the limit manual due to your system processor number 
+cpu=$(cat /proc/cpuinfo | grep -c processor)
 check=$(echo $load '>' $cpu | bc -l )
 if (( $check == 1 ))
 then
